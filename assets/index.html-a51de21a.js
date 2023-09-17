ccda6557e25a593dabe94dd60c1bfd4b36f36465
Zamoca42 <suntail2002@naver.com>
2023-09-17 09:55:39 +0000

    Deploying to gh-pages from @ Zamoca42/blog@ebff6f87f6bcfac9486dcddd5605f1aedb8b393a 🚀

---
+++ assets/index.html-a51de21a.js
@@ -0,0 +1,6 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o,c as t,a as n}from"./app-dfcf4e9b.js";const a={};function r(s,c){return o(),t("div",null,[n(` 
+This is an example of a project homepage. You can place your main content here.
+
+To use this layout, you need to set \`home: true\` in the page front matter.
+
+For related descriptions of configuration items, please see [Project HomePage Layout Config](https://theme-hope.vuejs.press/guide/layout/home/). `)])}const u=e(a,[["render",r],["__file","index.html.vue"]]);export{u as default};
