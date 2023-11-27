0d4b4700d2b9f890b104aafa4b92d0f8cca67899
Zamoca42 <suntail2002@naver.com>
2023-11-27 01:34:45 +0000

    Deploying to gh-pages from @ Zamoca42/blog@b119b7aa960e58fa7f52b23da0dec09af16753ed 🚀

---
+++ assets/pageview-00a3e0c3.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./app-cd06214a.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-cd06214a.js").then(r=>r.M),[]);return e({serverURL:"https://waline-comment-zamoca42s-projects.vercel.app/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
